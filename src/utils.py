676584528b9e435fb5948bbcc52de648e5633ca9
BbqGamer <korba.adam@gmail.com>
2023-12-26 22:58:44 +0100

    Create format to save goban state

--- src/utils.py
+++ src/utils.py
@@ -31,15 +31,47 @@ def draw_board(stones: list[Stone], board_size: int = 19):
         assert 0 <= x < board_size
         assert 0 <= y < board_size
 
-        ax.plot(x, y, 'o', markersize=30, markeredgecolor=(
+        ax.plot(x-1, y-1, 'o', markersize=30, markeredgecolor=(
             0, 0, 0), markerfacecolor=color, markeredgewidth=2)
+
+    ax.invert_yaxis()
+
     plt.show()
 
 
-if __name__ == "__main__":
-    stones = [
-        (10, 10, BLACK),
-        (11, 11, WHITE)
-    ]
+def read_stones(content: str) -> list[Stone]:
+    """Read stones from a string in the format:
+            2;12
+            4;7,8,17
+            6;16
 
+            3;6,7,12,15
+            15;4,17
+        Where first chunk are black stones and the second chunk are white stones.
+        first number in line indicates y coordinate and the rest are x coordinates.
+        min coordinate here is 1 and max 19 (not starting from 0)
+    """
+    stones = []
+    chunks = content.split('\n\n')
+    assert len(chunks) == 2
+
+    for chunk, color in zip(chunks, [BLACK, WHITE]):
+        for line in chunk.splitlines():
+            y, xs = line.split(';')
+            for x in xs.split(','):
+                stones.append(Stone(int(x), int(y), color))
+
+    return stones
+
+
+EXAMPLE_BOARD = """2;12
+4;7,8,17
+6;16
+
+3;6,7,12,15
+15;4,17"""
+
+
+if __name__ == "__main__":
+    stones = read_stones(EXAMPLE_BOARD)
     draw_board(stones)
